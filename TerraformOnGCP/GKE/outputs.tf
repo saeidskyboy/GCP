b761d548e693fc2df212ca41dd737ae43e136b04
saeid <saeid@seznam.cz>
2025-03-02 21:01:24 +0100

    update app-workflow and output.tf

--- TerraformOnGCP/GKE/outputs.tf
+++ TerraformOnGCP/GKE/outputs.tf
@@ -1,9 +1,10 @@
 # this output will pass info to make sure that indicates the cluster's existence
 # so we we don't need rerun this workflow in GH
+# TerraformOnGCP/GKE/outputs.tf
 output "cluster_name" {
-  value = google_container_cluster.primary.name
+  value = google_container_cluster.gke-cluster.name
 }
 
 output "cluster_region" {
-  value = google_container_cluster.primary.location
+  value = google_container_cluster.gke-cluster.location
 }
