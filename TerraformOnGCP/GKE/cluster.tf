5a417a709487afec31c38b4b2ae9b83c10d1657b
saeid <saeid@seznam.cz>
2025-03-06 00:50:41 +0100

    simplify cluster with one node to have fater process - change output variables

--- TerraformOnGCP/GKE/cluster.tf
+++ TerraformOnGCP/GKE/cluster.tf
@@ -1,42 +1,58 @@
 # resource doc: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster
 
 resource "google_container_cluster" "gke-cluster" {
-    name = "gke-cluster"
-    location = "us-central1-f" # two options: regional or zone if we specify a region then GKE will create high availability cluster cluster in multiple zones i nthat region
-    network = google_compute_network.vpc-gke.self_link
-    subnetwork = google_compute_subnetwork.private-subnet.self_link
-    remove_default_node_pool = true
-    initial_node_count = 1
-    #these two services (logging and monitoring can cost you alot, be careful and keep an eye on your billing)
-    logging_service = "logging.googleapis.com/kubernetes" # this option will deploy a fluentbit container to each node and forward logs to a google cloud loggin service
-    monitoring_service = "monitoring.googleapis.com/kubernetes"
-    networking_mode = "VPC_NATIVE"
-    # depends_on = [ google_project_service.container ]
-    addons_config {
-      http_load_balancing {
-        disabled = true
-      }
-      horizontal_pod_autoscaling {
-        disabled = false
-      }
-    }
-    release_channel {
-      channel = "REGULAR"
-    }
+  count = data.google_container_cluster.existing_cluster.status == "RUNNING" ? 0 : 1
 
-    workload_identity_config {
-      workload_pool = "cloud-2255.svc.id.goog"
-    }
+  name     = "gke-cluster"
+  location = "us-central1-f"
+  network      = google_compute_network.vpc-gke.self_link
+  subnetwork   = google_compute_subnetwork.private-subnet.self_link
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
-    ip_allocation_policy {
-      cluster_secondary_range_name = "k8s-pods-range"
-      services_secondary_range_name = "k8s-service-range"
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+  networking_mode    = "VPC_NATIVE"
+
+  addons_config {
+    http_load_balancing {
+      disabled = true
     }
-    # this will make our cluster private, this will use only private IPs from our private subnet for the k8s nodes
-    private_cluster_config {
-      enable_private_nodes = true
-      enable_private_endpoint = false # if we have a VPN setup to connect to k8s cluster we need set this option to "true"    
-    master_ipv4_cidr_block = "172.16.0.0/28" #this CIDR range is used for the k8s control plane and since google is managing the control plane in their network and establish a peering connection to our VPC we dont need to specify it
+    horizontal_pod_autoscaling {
+      disabled = false
     }
-    deletion_protection = false
+  }
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  workload_identity_config {
+    workload_pool = "cloud-2255.svc.id.goog"
+  }
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "k8s-pods-range"
+    services_secondary_range_name = "k8s-service-range"
+  }
+
+  private_cluster_config {
+    enable_private_nodes   = true
+    enable_private_endpoint = false # Set to true if using private endpoint
+    master_ipv4_cidr_block  = "172.16.0.0/28"
+  }
+
+  deletion_protection = false # CRUCIAL: Add this back
+}
+
+data "google_container_cluster" "existing_cluster" {
+  name     = "gke-cluster"
+  location = "us-central1-f"
+  project  = "cloud-2255"
+}
+
+resource "kubernetes_namespace" "argocd" {
+  count = data.google_container_cluster.existing_cluster.status == "RUNNING" ? 0 : 1
+  metadata {
+    name = "argocd"
+  }
 }
--- TerraformOnGCP/GKE/node-pools.tf
+++ TerraformOnGCP/GKE/node-pools.tf
@@ -1,61 +1,28 @@
 # resource doc: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
 
-# resource "google_service_account" "gke_node_sa" {
-#   account_id   = "gke-node-sa"
-#   display_name = "Service Account for GKE Node Pool"
-#   project = "cloud-2255"
-# }
-
-resource "google_container_node_pool" "general" {
-    name = "node-pool"
-    cluster = google_container_cluster.gke-cluster.id
-    node_count = 1
-
-    management {
-        auto_repair = true
-        auto_upgrade = true
-    }
-    node_config {
-        preemptible = true #it is better to set as falese if you have enough quota
-        machine_type = "e2-micro"
-        disk_size_gb = 10
-        labels = {
-            role = "general"
-        }
-
-        service_account = "terraform@cloud-2255.iam.gserviceaccount.com"
-        oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-        }
-}
-
-
-resource "google_container_node_pool" "spot" {
-  name = "spot-pool"
-  cluster = google_container_cluster.gke-cluster.id
+resource "google_container_node_pool" "primary_nodes" {
+  count              = data.google_container_cluster.existing_cluster.status == "RUNNING" ? 0 : 1
+  name               = "primary-node-pool"
+  cluster            = google_container_cluster.gke-cluster[0].id
+  location           = google_container_cluster.gke-cluster[0].location
+  initial_node_count = 1
 
   management {
-    auto_repair = true
+    auto_repair  = true
     auto_upgrade = true
   }
 
-  autoscaling {
-    min_node_count = 1
-    max_node_count = 5
-  }
-
   node_config {
-    preemptible = true
+    preemptible  = false
+    machine_type = "e2-medium"
+    disk_size_gb = 20
     labels = {
-     team = "devops"
-     }
-        # with this config block we avoid of accidental scheduling of nodes
-    taint {
-      key = "instance_type"
-      value = "spot"
-      effect = "PREFER_NO_SCHEDULE"
+      role = "general"
     }
 
     service_account = "terraform@cloud-2255.iam.gserviceaccount.com"
-        oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
-    }
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
