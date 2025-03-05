c10d9af5b8c76e5e264fe644f55981fc7c97c57c
saeid <saeid@seznam.cz>
2025-03-06 00:04:38 +0100

    separate workflows and make app-deploy workflow manual as well

--- TerraformOnGCP/GKE/outputs.tf
+++ TerraformOnGCP/GKE/outputs.tf
@@ -1,6 +1,7 @@
 # this output will pass info to make sure that indicates the cluster's existence
 # so we we don't need rerun this workflow in GH
 # TerraformOnGCP/GKE/outputs.tf
+# TerraformOnGCP/GKE/outputs.tf
 output "cluster_name" {
   value = google_container_cluster.gke-cluster.name
 }
