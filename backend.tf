536a79632cbc3c70e2933f2168bf1e8ce6c8550c
saeid <saeid@seznam.cz>
2025-03-06 22:28:59 +0100

    role back to initial set up and configs

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# #here is the backend.tf file configs for terraform, if you are willing store your state file in a bucket uncomment bellow codes and add your bucket name and prefix.
+# # #here is the backend.tf file configs for terraform, if you are willing store your state file in a bucket uncomment bellow codes and add your bucket name and prefix.
 
-terraform {
-  backend "gcs" {
-    bucket = "store-saeid-gke-statefiles-simu"
-    prefix = "gke/terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket = "store-saeid-gke-statefiles-simu"
+#     prefix = "gke/terraform.tfstate"
+#   }
+# }
