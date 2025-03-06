d1c3a7d8de029b299b1468796889bc75a235d8d4
saeid <saeid@seznam.cz>
2025-03-06 22:26:06 +0100

    role back to initial set up and configs

--- TerraformOnGCP/GKE/outputs.tf
+++ TerraformOnGCP/GKE/outputs.tf
@@ -1,9 +1,9 @@
 # this output will pass info to make sure that indicates the cluster's existence
 # so we we don't need rerun this workflow in GH
 output "cluster_name" {
-  value = element(concat(google_container_cluster.gke-cluster.name, [""]), 0)
+  value = google_container_cluster.gke-cluster.name
 }
 
 output "cluster_region" {
-  value = element(concat(google_container_cluster.gke-cluster.location, [""]), 0)
+  value = google_container_cluster.gke-cluster.location
 }
