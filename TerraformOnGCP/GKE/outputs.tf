57829168826596d1478d2630841666d6e6a363a8
saeid <saeid@seznam.cz>
2025-03-09 17:55:31 +0100

    integrate the nginx/lb/ to argocd workflow

--- TerraformOnGCP/GKE/outputs.tf
+++ TerraformOnGCP/GKE/outputs.tf
@@ -1,5 +1,5 @@
 # this output will pass info to make sure that indicates the cluster's existence
-# so we we don't need rerun this workflow in GH
+# so we we don't need rerun this workflow in GH 
 output "cluster_name" {
   value = google_container_cluster.gke-cluster.name
 }
