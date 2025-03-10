4d3f3754a9941d0deb60cefda990a626d7a040cf
saeid <saeid@seznam.cz>
2025-03-10 14:12:56 +0100

    fix: issue with recogn cluster ID

--- TerraformOnGCP/GKE/node-pools.tf
+++ TerraformOnGCP/GKE/node-pools.tf
@@ -1,8 +1,8 @@
 resource "google_container_node_pool" "primary_nodes" {
   count              = var.create_cluster ? 1 : 0
   name               = "primary-node-pool"
-  cluster            = google_container_cluster.gke-cluster[0].id
-  location           = google_container_cluster.gke-cluster[0].location
+  cluster            = google_container_cluster.gke-cluster.id
+  location           = google_container_cluster.gke-cluster.location
   initial_node_count = 1
 
   management {
