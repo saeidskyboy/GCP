6b94926ab067f397a17e741cab471ec32cf4d11d
saeid <saeid@seznam.cz>
2025-03-30 20:43:28 +0200

    project-metdata configs are not working, exclude it from the deploy and test the ssh between ansible controller node and GKE

--- TerraformOnGCP/k8s-argocd-monitoring-with-ansible/outputs.tf
+++ TerraformOnGCP/k8s-argocd-monitoring-with-ansible/outputs.tf
@@ -1,19 +1,19 @@
-output "cluster_name" {
-  value = google_container_cluster.gke-cluster.name
-}
+# output "cluster_name" {
+#   value = google_container_cluster.gke-cluster.name
+# }
 
-output "cluster_region" {
-  value = google_container_cluster.gke-cluster.location
-}
+# output "cluster_region" {
+#   value = google_container_cluster.gke-cluster.location
+# }
 
 
-# --- Output the public key for use elsewhere ---
-output "ansible_public_key_openssh" {
-  description = "The generated SSH public key for Ansible in OpenSSH format."
-  value       = tls_private_key.ansible_ssh.public_key_openssh
-}
+# # --- Output the public key for use elsewhere ---
+# output "ansible_public_key_openssh" {
+#   description = "The generated SSH public key for Ansible in OpenSSH format."
+#   value       = tls_private_key.ansible_ssh.public_key_openssh
+# }
 
-output "ansible_private_key_secret_version_id" {
-  description = "The resource ID of the secret version holding the private key."
-  value       = google_secret_manager_secret_version.ansible_private_key_version.id
-}
+# output "ansible_private_key_secret_version_id" {
+#   description = "The resource ID of the secret version holding the private key."
+#   value       = google_secret_manager_secret_version.ansible_private_key_version.id
+# }
