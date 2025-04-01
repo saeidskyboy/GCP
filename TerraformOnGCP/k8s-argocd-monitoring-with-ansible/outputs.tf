54e457cd40f11259b6d8353d9dc3d6de1cd8cf84
saeid <saeid@seznam.cz>
2025-04-01 22:53:31 +0200

    update workflow with auth path

--- TerraformOnGCP/k8s-argocd-monitoring-with-ansible/outputs.tf
+++ TerraformOnGCP/k8s-argocd-monitoring-with-ansible/outputs.tf
@@ -12,7 +12,7 @@ output "ssh_public_key_fingerprint" {
   value       = google_os_login_ssh_public_key.ssh_sa_key.fingerprint
 }
 
-# Output for Debugging and Access 
+# Output for Debugging and Access
 output "ssh_private_key_secret_name" {
   value       = google_secret_manager_secret.ansible_private_key_secret.name
   description = "The name of the Secret Manager secret storing the SSH private key"
