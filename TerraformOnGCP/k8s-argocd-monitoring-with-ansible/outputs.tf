fd4505a426b3b07e5fe72732dd05ca211efca5e6
saeid <saeid@seznam.cz>
2025-04-02 23:12:40 +0200

    out put update and syntax correction

--- TerraformOnGCP/k8s-argocd-monitoring-with-ansible/outputs.tf
+++ TerraformOnGCP/k8s-argocd-monitoring-with-ansible/outputs.tf
@@ -11,9 +11,3 @@ output "ssh_private_key_secret_name" {
   value       = google_secret_manager_secret.ansible_private_key_secret.name
   description = "The name of the Secret Manager secret storing the SSH private key"
 }
-
-output "os_login_status" {
-  description = "Status of OS Login enablement"
-  value = try(data.google_compute_project_metadata_item.os_login.value,
-  try(google_compute_project_metadata_item.os_login_enabled[0].value, "UNKNOWN"))
-}
