10319a429c634629fca6cc1e2a75600aea67aa14
saeid <saeid@seznam.cz>
2025-04-02 22:50:00 +0200

    update os-login

--- TerraformOnGCP/k8s-argocd-monitoring-with-ansible/outputs.tf
+++ TerraformOnGCP/k8s-argocd-monitoring-with-ansible/outputs.tf
@@ -6,7 +6,7 @@ output "cluster_region" {
   value = google_container_cluster.gke-cluster.location
 }
 
-# Output for Debugging and Access
+# Output for Debugging and Access 
 output "ssh_private_key_secret_name" {
   value       = google_secret_manager_secret.ansible_private_key_secret.name
   description = "The name of the Secret Manager secret storing the SSH private key"
